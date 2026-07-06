8407ee3c6612d692cc51506cd0c2a0434f627aff
Sruthi Sukumaran P <sruthipanaparambil@gmail.com>
2026-07-06 19:16:13 +0530

    Configure MySQL using environment variables

--- app.py
+++ app.py
@@ -106,12 +106,22 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.secret_key = os.getenv("SECRET_KEY")
+app.secret_key = os.getenv(
+    "SECRET_KEY",
+    "shopsmart_secret_key"
+)
 
 app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+
 app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+
 app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+
 app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+
+app.config["MYSQL_PORT"] = int(
+    os.getenv("MYSQL_PORT", 3306)
+)
 
 mysql = MySQL(app)
 
